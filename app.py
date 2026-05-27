b017e6422a717d1f07351f9ae4a56370c7a72caf
serguppp <serg.u@wp.pl>
2026-05-27 13:29:57 +0200

    Improve UI and clean code

--- app.py
+++ app.py
@@ -5,24 +5,23 @@ import time
 from components.button import Button
 from components.runner import CppRunner
 
+def map_list_to_string(string):
+    return  ", ".join(map(str, string))
+
 CPP_EXE_PATH = "./src/main"
 # Page settings
 st.set_page_config(page_title="Restriction Mapping Heuristics", layout="centered")
 st.title("Restriction Mapping Heuristics")
 
-# UI
-st.sidebar.header("Input Parameters")
-m = st.sidebar.slider("P size", min_value=5, max_value=100, value=10)
-max_val = st.sidebar.number_input("Max Distance Value", min_value=10, max_value=1000, value=100)
 
 if "p_points" not in st.session_state:
-    st.session_state.p_points = []
+    st.session_state.p_points = ""
 if "d_distances" not in st.session_state:
-    st.session_state.d_distances = []
+    st.session_state.d_distances = ""
 if "m_value" not in st.session_state:
     st.session_state.m_value = ""
 if "p_result" not in st.session_state:
-    st.session_state.p_result = []
+    st.session_state.p_result = ""
 if "success_msg" not in st.session_state:
     st.session_state.success_msg = ""
 
@@ -30,12 +29,27 @@ if st.session_state.success_msg:
     st.success(st.session_state.success_msg)
     st.session_state.success_msg = ""
 
+runner = CppRunner(CPP_EXE_PATH)
+button = Button(runner)
+
+# UI
+with st.sidebar:
+    st.header("Input Parameters")
+    m = st.slider("P size", min_value=5, max_value=100, value=10)
+    max_val = st.number_input("Max Distance Value", min_value=10, max_value=1000, value=100)
+    
+    st.markdown("---")  
+    st.header("Actions")
+
+    button.render_generate_p_and_d(m, max_val)
+    button.render_generate_d(st.session_state.p_points)
+    button.render_run_heuristics(st.session_state.p_points, st.session_state.d_distances)
+
 col1, col2, col3 = st.columns(3)
-                
+
 with col1:
     st.subheader("Generated P Points")
-    #p_points = json.dumps(st.session_state.p_points) if st.session_state.p_points else ""
-    p_points = ", ".join(map(str, st.session_state.p_points)) if st.session_state.p_points else ""
+    p_points = st.session_state.p_points if st.session_state.p_points else ""
     p_text_area = st.text_area(
         label = "Generated P Points",
         value = p_points,
@@ -44,8 +58,7 @@ with col1:
     
 with col2:
     st.subheader("Set of D Distances")
-    #d_distances = json.dumps(st.session_state.d_distances) if st.session_state.d_distances else ""
-    d_distances = ", ".join(map(str, st.session_state.d_distances)) if st.session_state.d_distances else "" 
+    d_distances = st.session_state.d_distances if st.session_state.d_distances else "" 
     d_text_area = st.text_area(
         label = "Generated D Distances",
         value = d_distances,
@@ -58,18 +71,11 @@ with col3:
     if st.session_state.m_value != "":
         st.metric(label="Found P Size (m)", value=st.session_state.m_value)
         
-    p_res = ", ".join(map(str, st.session_state.p_result)) if st.session_state.p_result else ""
+    p_res = st.session_state.p_result if st.session_state.p_result else ""
     st.text_area(
         label="Result P Points",
         value=p_res,
         height=150,
-        disabled=True, 
+        disabled=True
     )
 
-
-runner = CppRunner(CPP_EXE_PATH)
-ui = Button(runner)
-
-ui.render_generate_p_and_d(m, max_val)
-ui.render_generate_d(p_text_area)
-ui.render_run_heuristics(p_text_area, d_text_area)
--- components/button.py
+++ components/button.py
@@ -4,14 +4,17 @@ import json
 import time
 
 
-def convert_text_to_list(text_area):
-    return [int(x.strip()) for x in text_area.split(",") if x.strip()]
+def map_text_to_list(text):
+    return [int(x.strip()) for x in text.split(",") if x.strip()]
+
+def map_list_to_string(list):
+    return  ", ".join(map(str, list))
 
 class Button():
     def __init__(self, runner):
         self.runner = runner
     
-    def render_generate_p_and_d(self, m, max_val):
+    def render_generate_p_and_d(self, m, max_val:str):
         if st.button("Generate P and D", type="primary"):
             start = time.time()
 
@@ -19,29 +22,29 @@ class Button():
             data = json.loads(process.stdout)
 
             if data["status"] == "success":
-                st.session_state.p_points = data["p_points"]
-                st.session_state.d_distances = data["d_distances"]
+                st.session_state.p_points = map_list_to_string(data["p_points"])
+                st.session_state.d_distances = map_list_to_string(data["d_distances"])
                 st.session_state.success_msg = f"Data generated successfully in {time.time() - start:.4f}s!"
                 st.rerun()
 
-    def render_generate_d(self, p_text_area):
+    def render_generate_d(self, p_text_area:str):
         if st.button("Generate D"):
             if not p_text_area.strip():
                 st.warning("P vector is empty")
                 return
 
-            p_list = convert_text_to_list(p_text_area)
+            p_list = map_text_to_list(p_text_area)
             process = self.runner.run_generate_d(p_list)
             data = json.loads(process.stdout)
 
             if data["status"] == "success":
-                st.session_state.d_distances = data["d_distances"]
+                st.session_state.d_distances = map_list_to_string(data["d_distances"])
                 st.rerun()
 
-    def render_run_heuristics(self, p_text_area, d_text_area):
+    def render_run_heuristics(self, p_text_area:str, d_text_area:str):
         if st.button("Run Algorithm"):
-            p_list = convert_text_to_list(p_text_area)
-            d_list = convert_text_to_list(d_text_area)
+            p_list = map_text_to_list(p_text_area)
+            d_list = map_text_to_list(d_text_area)
 
             st.subheader("Algorithm Progress")
             status_text = st.empty()
@@ -57,7 +60,7 @@ class Button():
                 
             if data["status"] == "success":
                 st.session_state.m_value = data["m_value"]
-                st.session_state.p_result = data["p_result"]
+                st.session_state.p_result = map_list_to_string(data["p_result"])
                 st.session_state.success_msg = "Algorithm finished successfully!"
                 st.rerun()
  
